dd6f2185e14bb9a60b99758e79f3fbdc3beb81a5
SaulVVelazquez <1721110050@utectulancingo.edu.mx>
2022-10-05 15:59:31 +0000

    errores

--- API/main.py
+++ API/main.py
@@ -1,64 +1,100 @@
-
 from fastapi import FastAPI
 import sqlite3
 from typing import List
 from pydantic import BaseModel
-from fastapi import HTTPException,status
+from pydantic import EmailStr
+from fastapi import HTTPException
+from fastapi import status
 
+class mensaje(BaseModel):
+	mensaje: str
 
-class Mensaje(BaseModel):
-	mensaje:str
+class contactos(BaseModel):
+	id_contacto: int
+	nombre: str
+	email: str
+	telefono: str
 
-class Contactos(BaseModel):
-	id_contacto:int
-	nombre:str
-	email:str
-	telefono:str
+class contactosIN(BaseModel):
+	nombre: str
+	email: str
+	telefono: str
 
-description= """
-	#Contactos Api REST
-	API para crar un CRUD
-	de la tabla contactos
+description = """
+	# Contactos  API REST
+	API para crear un CRUD de la tabla contactos 
 	"""
-app= Fast API(
-	title="contactos API REST",
-	description=description,
-	version= "0.1",
-	contact= {"name":"Saul Valde3rrama Velazquez",
-		"email":"1721110050@utectulancingo.edu.mx"
-	}
-			
+app = FastAPI(
+	tittle = "Contactos API REST",
+	description = description,
+	version = "0.0.1",
+	terms_of_service="http://example.com/terms/",
+	contact = {
+		"name": "saul",
+		"email": "saul_736@hotmail.com",
+	},
+	license_info={
+		"name":"Apache 2.0",
+		"url":"https://www.apache.org/licenses/LICENSE-2.0.html",
+	},
 )
 
-@app.get(
+@app.get (
 	"/",
-	response_model= Mensaje,
-	status_code= status.HTTP_202_ACCEPTED,
-	summary="endpoint principal",
-	description="regresar mensaje de bienvenida",
+	response_model=mensaje,
+	status_code=status.HTTP_202_ACCEPTED,
+	summary="Endpoint principal",
+	description=" Regresa un mensaje de bienvenida",
 )
-async dif read_root():
-	response={"mensaje":"version 0.1"}
+
+async def read_root ():
+	response = {"mensaje": "version 0.1"}
 	return response
 
-@app.get(
-	"/contactos/",
-	response_model= List[Contactos],
-	status_code= status.HTTP_202_ACCEPTED,
-	summary="lista de contactos",
-	description="endpoint que regresara un array con todos los contactos",
+@app.get (
+	"/contactos/{id_contacto}",
+	response_model=contactosIN,
+	status_code=status.HTTP_202_ACCEPTED,
+	summary="Contacto a obtener",
+	description="Endpoint que regresa un array con el id de un solo contactos",
 )
-async dif get_contactos():
+
+async def get_contactos(id_contacto:int):
 	try:
 		with sqlite3.connect("API/sql/contactos.db") as connection:
-		connection.row_factory=sqlite3.Row
-		cursor=connection.cursor()
-		cursor.execute("SELECT  id_contacto,nombre,email,telefono FROM contactos;")
-		response=cursor.Fetchall()
-		return response
-	excecpt Exeception as error:
-		print (f "Error en get_cotactos {error.asgs}")
+			connection.row_factory = sqlite3.Row
+			cursor = connection.cursor()
+			sql="SELECT nombre,email,telefono FROM contactos WHERE id_contacto=?;"
+			values=(id_contacto, )
+			cursor.execute(sql,values)
+			response= cursor.fetchone()
+			return response
+	except Exception as error:
+		print(f"Error interno: {error.args}")
 		raise HTTPException(
-		status_code=status.HTTP_400_BAD_REQUEST,
-		detail="Error al consultar los datos"
-		)
+			status_code = status.HTTP_400_BAD_REQUEST,
+			detail = "Error al consultar los datos",
+		) 	
+
+@app.get (
+	"/contactos/",
+	response_model = List[contactos],
+	status_code = status.HTTP_202_ACCEPTED,
+	summary = "Lista de contactos",
+	description = "Endpoint que regresa un array con todos los contactos",
+)
+
+async def get_contactos():
+	try:
+		with sqlite3.connect("API/sql/contactos.db") as connection:
+			connection.row_factory = sqlite3.Row
+			cursor = connection.cursor()
+			cursor.execute("SELECT id_contacto, nombre, email, telefono FROM contactos;")
+			response = cursor.fetchall()
+			return response
+	except Exception as error:
+		print(f"Error interno: {error.args}")
+		raise HTTPException(
+			status_code = status.HTTP_400_BAD_REQUEST,
+			detail = "Error al consultar los datos",
+		) 				
--- API/sql/contactos.sql
+++ API/sql/contactos.sql
@@ -1,11 +1,13 @@
-
-
+CREATE database contactos;
+ 
 CREATE TABLE contactos(
 	id_contacto integer PRIMARY KEY AUTOINCREMENT,
-	nombre varchar (30),
-	email varchar (50),
-	telefono varchar (50)
+	nombre varchar (30) not null,
+	email varchar (50) not null unique,
+	telefono varchar (50) not null
 );
 
 INSERT INTO contactos (nombre,email,telefono) VALUES ("Saul","saul_736@hotmail.com","5584485068");
 INSERT INTO contactos (nombre,email,telefono) VALUES ("Itzel","itzelpv@email.com","555476021");
+
+SELECT * FROM  contactos;
