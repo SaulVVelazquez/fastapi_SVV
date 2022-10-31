052d153a9a9a3fd5ba9089c81d941ecb23481695
Saul <1721110050@utectulancingo.edu.mx>
2022-10-31 11:21:44 -0600

    ya quedo el consumo de la API

--- API/main.py
+++ API/main.py
@@ -198,8 +198,8 @@ async def delete_contactos(id_contacto: int):
 
 origins = [
     "http://localhost:8000",
-    "http:127.0.0.1:3000",
-     "http:127.0.0.1:8080",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8080",
 ]
 
 app.add_middleware(
